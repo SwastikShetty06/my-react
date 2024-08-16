98616ceb0e14fd27da5f5e6002040e01942935a5
SWASTIK SHETTY <swastikshetty@SWASTIKs-MacBook-Air.local>
2024-08-16 11:03:48 +0530

    add: initial files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero--photo" />
+            <h1 className="hero--header">Plan Your Trip Now</h1>
+            <p className="hero--text">"Journey Beyond, Explore the Unseen – Your Adventure Awaits!"<br />
+            Welcome to SLOTstay, where every journey becomes a cherished memory. From serene beach escapes to thrilling adventures, we design personalized trips just for you. Our expert team handles the details, so you can focus on enjoying every moment. Your perfect getaway starts here!
+            </p>
+        </section>
+    )
+}
---
+++ src/data.jsx
@@ -0,0 +1,119 @@
+export default [
+    {
+        id: 1,
+        title: "Bicycle ride in the mountains",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "../public/images/9.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Mumbai",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Stay in a five star resort",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "../public/images/2.jpeg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Goa",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Taj Mahal, one of the wonders",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/3.jpeg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 20
+        },
+        location: "India",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Mangalore Beach experience",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/4.jpeg",
+        stats: {
+            rating: 4.2,
+            reviewCount: 25
+        },
+        location: "Karnataka",
+        openSpots: 3,
+    },
+    {
+        id: 5,
+        title: "Murudeshwara Temple Gokarna",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/5.jpeg",
+        stats: {
+            rating: 4.6,
+            reviewCount: 30
+        },
+        location: "Gokarna",
+        openSpots: 3,
+    },
+    {
+        id: 6,
+        title: "Bike ride to Ladakh",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/6.jpeg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 100
+        },
+        location: "India",
+        openSpots: 3,
+    },
+    {
+        id: 7,
+        title: "Bike ride to Kanyakumari",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/7.jpeg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 26
+        },
+        location: "Tamil Nadu",
+        openSpots: 3,
+    },
+    {
+        id: 8,
+        title: "Waterfall in Vasai",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/8.jpeg",
+        stats: {
+            rating: 4.6,
+            reviewCount: 16
+        },
+        location: "Mumbai",
+        openSpots: 3,
+    },
+    {
+        id: 9,
+        title: "Boating in the lakes of Srinagar",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "../public/images/1.jpeg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Jammu Kashmir",
+        openSpots: 3,
+    }
+]
