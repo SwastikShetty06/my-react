ecb3e75a920944f7dd223bcc5a4741bf431a8638
Swastik Shetty <127413152+SwastikShetty06@users.noreply.github.com>
2024-08-18 15:49:19 +0530

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -4,7 +4,7 @@ export default [
         title: "Bicycle ride in the mountains",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "../public/images/9.png",
+        coverImg: "../images/9.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -17,7 +17,7 @@ export default [
         title: "Stay in a five star resort",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "../public/images/2.jpeg",
+        coverImg: "../images/2.jpeg",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -30,7 +30,7 @@ export default [
         title: "Taj Mahal, one of the wonders",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/3.jpeg",
+        coverImg: "../images/3.jpeg",
         stats: {
             rating: 4.8,
             reviewCount: 20
@@ -43,7 +43,7 @@ export default [
         title: "Mangalore Beach experience",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/4.jpeg",
+        coverImg: "../images/4.jpeg",
         stats: {
             rating: 4.2,
             reviewCount: 25
@@ -56,7 +56,7 @@ export default [
         title: "Murudeshwara Temple Gokarna",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/5.jpeg",
+        coverImg: "../images/5.jpeg",
         stats: {
             rating: 4.6,
             reviewCount: 30
@@ -69,7 +69,7 @@ export default [
         title: "Bike ride to Ladakh",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/6.jpeg",
+        coverImg: "../images/6.jpeg",
         stats: {
             rating: 4.9,
             reviewCount: 100
@@ -82,7 +82,7 @@ export default [
         title: "Bike ride to Kanyakumari",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/7.jpeg",
+        coverImg: "../images/7.jpeg",
         stats: {
             rating: 4.8,
             reviewCount: 26
@@ -95,7 +95,7 @@ export default [
         title: "Waterfall in Vasai",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/8.jpeg",
+        coverImg: "../images/8.jpeg",
         stats: {
             rating: 4.6,
             reviewCount: 16
@@ -108,7 +108,7 @@ export default [
         title: "Boating in the lakes of Srinagar",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../public/images/1.jpeg",
+        coverImg: "../images/1.jpeg",
         stats: {
             rating: 4.8,
             reviewCount: 2
